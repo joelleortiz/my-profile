b578aa038315dc104dfd5ee24f302d59b476b7a8
joelleortiz <josephineanngabrielleortiz@gmail.com>
2017-09-06 00:03:41 +0800

    added typing effect

---
+++ contents/scripts/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".subheader").typed({
+        strings: ["Programmer", "Traveler", "Foodie", "Dreamer", "Audiophile"],
+        typeSpeed: 1,
+        loop: true,
+        loopCount: Infinity,
+        shuffle: true
+    });
+});
