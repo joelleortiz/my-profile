8cd6082439cdc8b9420f98fd228cba0b91f06418
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2021-11-11 23:03:11 +1100

    migrate to sveltekit + tailwindcss

---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import bg from '$lib/assets/sayagata-400px.png';
+</script>
+
+<div
+	class="h-screen relative flex flex-col justify-center bg-white bg-repeat"
+	style="background-image: url('{bg}')"
+>
+	<header class="container mx-auto max-w-screen-md">
+		<h1 class="text-6xl p12">Hi, I'm Joelle.</h1>
+		<div class="h-20 my-10 ml-2">
+			<span class="subheader"></span>
+		</div>
+	</header>
+	<section class="absolute bottom-0 right-0 p-12">
+		<ul class="border-dashed border-gray-900 border-opacity-50 border-t border-b p-4">
+			<li><i class="fa fa-mobile mx-2" /> <a href="tel:+61481091955">+61 481 091 955</a></li>
+			<li>
+				<i class="fa fa-envelope mr-2" />
+				<a href="mailto:contact@joelleortiz.me">contact@joelleortiz.me</a>
+			</li>
+			<li>
+				<i class="fa fa-mouse-pointer mx-2" /> <a href="http://joelleortiz.me">joelleortiz.me</a>
+			</li>
+		</ul>
+	</section>
+</div>
